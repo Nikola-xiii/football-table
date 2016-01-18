0a750ee1d601535448ed0aacb9c8ebdc7b7d1313
Vladyk750 <fan.of.Dnipro@gmail.com>
2016-01-18 18:46:08 +0200

    Add object for team
    Javascript

--- script.js
+++ script.js
@@ -1,11 +1,522 @@
-var variable1 = true;
-var variable2 = 15;
-var variable3 = "asdasdasd";
-var variable4 = true;
+var Dynamo = {                                                                                  place:"2", 
+             name:"Dynamo",
+             logo:"", 
+             games:"16",
+             win:"14", 
+             draw:"1", 
+             lose:"1",
+             goal_difference:"37/6",
+             score:"43"
+};
+var Shakhtar = {                                                                                place:"1", 
+             name:"Shakhtar",
+             logo:"", 
+             games:"16",
+             win:"14", 
+             draw:"1", 
+             lose:"1",
+             goal_difference:"50/11",
+             score:"43"
+};
+var Zorya = {                                                                                  place:"3", 
+             name:"Zorya",
+             logo:"", 
+             games:"16",
+             win:"10", 
+             draw:"4", 
+             lose:"2",
+             goal_difference:"32/14",
+             score:"34"
+};
+var Dnipro = {                                                                                  place:"4", 
+             name:"Dnipro",
+             logo:"", 
+             games:"16",
+             win:"8", 
+             draw:"4", 
+             lose:"4",
+             goal_difference:"28/15",
+             score:"28"
+};
+var Volin = {                                                                                  place:"5", 
+             name:"Volyn",
+             logo:"", 
+             games:"16",
+             win:"18", 
+             draw:"3", 
+             lose:"5",
+             goal_difference:"26/23",
+             score:"27"
+};
+var Vorskla = {                                                                                place:"6", 
+             name:"Vorskla",
+             logo:"", 
+             games:"16",
+             win:"6", 
+             draw:"7", 
+             lose:"3",
+             goal_difference:"20/19",
+             score:"25"
+};
+var Hoverla = {                                                                                place:"13", 
+             name:"Hoverla",
+             logo:"", 
+             games:"16",
+             win:"1", 
+             draw:"7", 
+             lose:"8",
+             goal_difference:"11/26",
+             score:"10"
+};
+var Olexandria = {                                                                              place:"7", 
+             name:"Olexandria",
+             logo:"", 
+             games:"16",
+             win:"6", 
+             draw:"4", 
+             lose:"6",
+             goal_difference:"19/18",
+             score:"22"
+};
+var Stal = {                                                                                    place:"9", 
+             name:"Stal",
+             logo:"", 
+             games:"16",
+             win:"4", 
+             draw:"4", 
+             lose:"8",
+             goal_difference:"14/22",
+             score:"16"
+}; 
+var Metalurg = {                                                                                place:"14", 
+             name:"Metalurg",
+             logo:"", 
+             games:"16",
+             win:"0", 
+             draw:"3", 
+             lose:"13",
+             goal_difference:"7/50",
+             score:"3"
+};
+var Metalist = {                                                                                place:"11", 
+             name:"Metalist",
+             logo:"", 
+             games:"16",
+             win:"1", 
+             draw:"8", 
+             lose:"7",
+             goal_difference:"12/25",
+             score:"11"
+};
+var Chornomorets = {                                                                            place:"12", 
+             name:"Chornomorets",
+             logo:"", 
+             games:"16",
+             win:"1", 
+             draw:"7", 
+             lose:"8",
+             goal_difference:"15/28",
+             score:"10"
+};
+var Olimpik = {                                                                                place:"10", 
+             name:"Olimpik",
+             logo:"", 
+             games:"16",
+             win:"4", 
+             draw:"2", 
+             lose:"10",
+             goal_difference:"16/25",
+             score:"14"
+};
+var Karpaty = {                                                                                place:"8", 
+             name:"Karpaty",
+             logo:"", 
+             games:"16",
+             win:"6", 
+             draw:"3", 
+             lose:"7",
+             goal_difference:"18/23",
+             score:"21"
+};
 
-console.log(typeof(variable4));
+var Leicester = {                                                                              place:"1", 
+             name:"Leicester",
+             logo:"", 
+             games:"22",
+             win:"12", 
+             draw:"8", 
+             lose:"2",
+             goal_difference:"39/26",
+             score:"44"
+};
+var Arsenal = {                                                                                place:"3", 
+             name:"Arsenal",
+             logo:"", 
+             games:"21",
+             win:"13", 
+             draw:"4", 
+             lose:"4",
+             goal_difference:"37/21",
+             score:"43"
+};
+var Manchester_City = {                                                                        place:"2", 
+             name:"Manchester_City",
+             logo:"", 
+             games:"22",
+             win:"13", 
+             draw:"4", 
+             lose:"5",
+             goal_difference:"43/21",
+             score:"43"
+};
+var West_Ham = {                                                                                place:"6", 
+             name:"West_Ham",
+             logo:"", 
+             games:"22",
+             win:"9", 
+             draw:"8", 
+             lose:"5",
+             goal_difference:"34/26",
+             score:"35"
+};
+var Stoke_City = {                                                                              place:"7", 
+             name:"Stoke_City",
+             logo:"", 
+             games:"21",
+             win:"9", 
+             draw:"5", 
+             lose:"7",
+             goal_difference:"24/22",
+             score:"32"
+};
+var Liverpool = {                                                                              place:"9", 
+             name:"Liverpool",
+             logo:"", 
+             games:"22",
+             win:"8", 
+             draw:"7", 
+             lose:"7",
+             goal_difference:"25/28",
+             score:"31"
+};
+var Everton = {                                                                                place:"11", 
+             name:"Everton",
+             logo:"", 
+             games:"22",
+             win:"6", 
+             draw:"11", 
+             lose:"5",
+             goal_difference:"39/32",
+             score:"29"
+};
+var West_Bromwich = {                                                                          place:"13", 
+             name:"West_Bromwich",
+             logo:"", 
+             games:"22",
+             win:"7", 
+             draw:"6", 
+             lose:"9",
+             goal_difference:"22/30",
+             score:"27"
+};
+var Chelsea = {                                                                                place:"14", 
+             name:"Chelsea",
+             logo:"", 
+             games:"22",
+             win:"6", 
+             draw:"7", 
+             lose:"9",
+             goal_difference:"31/34",
+             score:"25"
+};
+var Manchester_United = {                                                                      place:"5", 
+             name:"Manchester_United",
+             logo:"", 
+             games:"22",
+             win:"10", 
+             draw:"7", 
+             lose:"5",
+             goal_difference:"28/20",
+             score:"37"
+};
+var Watford = {                                                                                place:"12", 
+             name:"Watford",
+             logo:"", 
+             games:"21",
+             win:"8", 
+             draw:"5", 
+             lose:"8",
+             goal_difference:"25/24",
+             score:"29"
+};
+var Southgampton = {                                                                            place:"10", 
+             name:"Dynamo",
+             logo:"", 
+             games:"22",
+             win:"8", 
+             draw:"6", 
+             lose:"8",
+             goal_difference:"31/24",
+             score:"30"
+};
+var Tottenham = {                                                                              place:"4", 
+             name:"Tottenham",
+             logo:"", 
+             games:"22",
+             win:"10", 
+             draw:"9", 
+             lose:"3",
+             goal_difference:"38/18",
+             score:"39"
+};
+var Crystal_Palace = {                                                                          place:"8", 
+             name:"Crystal_Palace",
+             logo:"", 
+             games:"22",
+             win:"9", 
+             draw:"4", 
+             lose:"9",
+             goal_difference:"23/24",
+             score:"31"
+};
+var Swansea_City = {                                                                            place:"18", 
+             name:"Swansea",
+             logo:"", 
+             games:"21",
+             win:"4", 
+             draw:"7", 
+             lose:"10",
+             goal_difference:"19/30",
+             score:"19"
+};
+var Norwich_City = {                                                                            place:"16", 
+             name:"Norwich",
+             logo:"", 
+             games:"22",
+             win:"5", 
+             draw:"6", 
+             lose:"11",
+             goal_difference:"24/38",
+             score:"23"
+};
+var Bournemouth = {                                                                            place:"15", 
+             name:"Bournemouth",
+             logo:"", 
+             games:"22",
+             win:"6", 
+             draw:"6", 
+             lose:"10",
+             goal_difference:"26/37",
+             score:"24"
+};
+var Aston_Villa = {                                                                            place:"20", 
+             name:"Aston_Villa",
+             logo:"", 
+             games:"22",
+             win:"2", 
+             draw:"6", 
+             lose:"14",
+             goal_difference:"18/38",
+             score:"12"
+};
+var Sunderland = {                                                                              place:"19", 
+             name:"Sunderland",
+             logo:"", 
+             games:"22",
+             win:"5", 
+             draw:"3", 
+             lose:"14",
+             goal_difference:"27/45",
+             score:"18"
+};
+var Newcastle_United = {                                                                        place:"17", 
+             name:"Newcastle",
+             logo:"", 
+             games:"22",
+             win:"5", 
+             draw:"6", 
+             lose:"11",
+             goal_difference:"24/39",
+             score:"21"
+};
 
-
-if (name < 5) {
-    console.log("Не")
-}
+var Bayern = {                                                                                  place:"1", 
+             name:"Bayern",
+             logo:"", 
+             games:"17",
+             win:"15", 
+             draw:"1", 
+             lose:"1",
+             goal_difference:"46/8",
+             score:"46"
+};
+var Bayer = {                                                                                  place:"5", 
+             name:"Bayer",
+             logo:"", 
+             games:"17",
+             win:"8", 
+             draw:"3", 
+             lose:"6",
+             goal_difference:"25/20",
+             score:"27"
+};
+var Borussia_D = {                                                                              place:"2", 
+             name:"Borussia_D",
+             logo:"", 
+             games:"17",
+             win:"12", 
+             draw:"2", 
+             lose:"3",
+             goal_difference:"47/23",
+             score:"38"
+};
+var Borussia_M = {                                                                              place:"4", 
+             name:"Borussia_M",
+             logo:"", 
+             games:"17",
+             win:"9", 
+             draw:"2", 
+             lose:"6",
+             goal_difference:"34/30",
+             score:"29"
+};
+var Wolfsburg = {                                                                              place:"7", 
+             name:"Wolfsburg",
+             logo:"", 
+             games:"17",
+             win:"7", 
+             draw:"5", 
+             lose:"5",
+             goal_difference:"26/21",
+             score:"26"
+};
+var Werder = {                                                                                  place:"16", 
+             name:"Werder",
+             logo:"", 
+             games:"17",
+             win:"4", 
+             draw:"3", 
+             lose:"10",
+             goal_difference:"17/32",
+             score:"15"
+};
+var Hamburger = {                                                                              place:"10", 
+             name:"Hamburger",
+             logo:"", 
+             games:"17",
+             win:"6", 
+             draw:"4", 
+             lose:"7",
+             goal_difference:"19/23",
+             score:"22"
+};
+var Koln = {                                                                                    place:"9", 
+             name:"Koln",
+             logo:"", 
+             games:"17",
+             win:"6", 
+             draw:"6", 
+             lose:"5",
+             goal_difference:"18/21",
+             score:"24"
+};
+var Herta = {                                                                                  place:"3", 
+             name:"Herta",
+             logo:"", 
+             games:"17",
+             win:"10", 
+             draw:"2", 
+             lose:"5",
+             goal_difference:"26/18",
+             score:"32"
+};
+var Schallke = {                                                                                place:"6", 
+             name:"Schallke",
+             logo:"", 
+             games:"17",
+             win:"8", 
+             draw:"3", 
+             lose:"6",
+             goal_difference:"23/23",
+             score:"27"
+};
+var Ingolstadt = {                                                                              place:"11", 
+             name:"Ingolstadt",
+             logo:"", 
+             games:"17",
+             win:"5", 
+             draw:"5", 
+             lose:"7",
+             goal_difference:"11/18",
+             score:"20"
+};
+var Mainz = {                                                                                  place:"8", 
+             name:"Mainz",
+             logo:"", 
+             games:"17",
+             win:"7", 
+             draw:"3", 
+             lose:"7",
+             goal_difference:"23/23",
+             score:"24"
+};
+var Darmstadt = {                                                                              place:"13", 
+             name:"Darmstadt",
+             logo:"", 
+             games:"17",
+             win:"4", 
+             draw:"6", 
+             lose:"7",
+             goal_difference:"17/26",
+             score:"18"
+};
+var Eintracht = {                                                                              place:"14", 
+             name:"Eintracht",
+             logo:"", 
+             games:"17",
+             win:"4", 
+             draw:"5", 
+             lose:"8",
+             goal_difference:"21/28",
+             score:"17"
+};
+var Hoffenheim = {                                                                              place:"18", 
+             name:"Hoffenheim",
+             logo:"", 
+             games:"17",
+             win:"2", 
+             draw:"7", 
+             lose:"8",
+             goal_difference:"17/25",
+             score:"13"
+};
+var Augsburg = {                                                                                place:"12", 
+             name:"Augsburg",
+             logo:"", 
+             games:"17",
+             win:"5", 
+             draw:"4", 
+             lose:"8",
+             goal_difference:"21/26",
+             score:"19"
+};
+var Hannover = {                                                                                place:"17", 
+             name:"Hannover",
+             logo:"", 
+             games:"17",
+             win:"4", 
+             draw:"2", 
+             lose:"11",
+             goal_difference:"18/29",
+             score:"14"
+};
+var Stuttgart = {                                                                              place:"15", 
+             name:"Stuttgart",
+             logo:"", 
+             games:"17",
+             win:"4", 
+             draw:"3", 
+             lose:"10",
+             goal_difference:"22/37",
+             score:"15"
+};
